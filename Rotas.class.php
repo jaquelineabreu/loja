3f8932f93c14daf106fed61d62b0008df60e6a29
Jaqueline Abreu <59976892+jaquelineabreu@users.noreply.github.com>
2020-12-03 21:53:16 -0300

    Add files via upload

--- Rotas.class.php
+++ Rotas.class.php
@@ -3,10 +3,21 @@
 	/*classe estatica consigo chamar mesmo sem instaciar a classe*/
 	Class Rotas{
 
+		public static $pag;
+
 		static function get_pagina(){
 			/*se foi chamado na url*/
 			if(isset($_GET['pag'])){
-				$pagina = 'controller/' .$_GET['pag'] . '.php';
+
+				$pagina = $_GET['pag'];
+
+				//mesma coisa que o this
+				self::$pag = explode('/', $pagina);
+				//var_dump(self::$pag);
+
+
+				$pagina = 'controller/' .self::$pag[0] . '.php';
+				//$pagina = 'controller/' .$_GET['pag'] . '.php';
 				
 				/*Verificar se existe*/
 				if(file_exists($pagina)){
